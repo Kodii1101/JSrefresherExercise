c786e25d49aca0deae3905109ad079443c9041e3
Jonnell Lazaro J. Bingat <jlbversusxiii@gmail.com>
2024-02-21 03:30:16 +0800

    Ended 40 minute mark on tutorial.

---
+++ main.js
@@ -0,0 +1,15 @@
+ var number = `'lets try "double quote this"'`;
+ const tryLang = document.querySelector("#try");
+ /* rwe
+ rwer
+ wer
+ wewe
+ rwe
+ rwe
+  
+ multi line comment */
+
+
+console.log(number = "sure oi");
+
+tryLang.textContent = number[number.length-1];
