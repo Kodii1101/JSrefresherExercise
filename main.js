7edc09d39936ebfe5f6db5040d5e662b8ec67a67
Jonnell Lazaro J. Bingat <jlbversusxiii@gmail.com>
2024-03-02 04:42:28 +0800

    Another Jacascript exercise/review/learning

--- main.js
+++ main.js
@@ -8,23 +8,29 @@ console.log(removedArray);
 console.log(myArray);
  */
 
-/* function taxThis (a,b){
-   const c = .12;
-   var ac=a*c;
-   var bc=b*c;
-   var ab=a+b;
+/* function taxThis(a, b) {
+    const c = 0.12;
+    var ac = a * c;
+    var bc = b * c;
+    var ab = a + b;
 
     return ac + " " + bc + " " + ab;
 }
 
-console.log(taxThis(3500,4500));
+console.log(taxThis(3500, 4500));
 
 var testThis = {
     x: "x ni bhai",
     y: "y ni bhai",
-    z: "z ni bhai"
+    z: "z ni bhai",
 };
 
 console.log(testThis.x); */
+
+/* function xyz(shiwq) {
+    dasdas;
+    dasdasdsa;      
+
+} */
 
 // IM DONE! 3:26:42 thank you Vanzkinz and freeCodeCamp! https://www.youtube.com/watch?v=PkZNo7MFNFg&ab_channel=freeCodeCamp.org
